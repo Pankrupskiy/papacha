58b9045ba78e59dbef451f94e28523d272d3adea
Uladzislau <krupskiy557@gmail.com>
2022-09-04 15:57:13 +0300

    задача 8

---
+++ задачи/задача 2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите трейтее число: ");
+int number3 = int.Parse(Console.ReadLine());
+int Mmax = number1;
+
+if (number1 > number2) 
+{
+    number1 = Mmax;
+}
+else
+{
+    number2 = Mmax;
+}
+if (number2 > number3)
+{
+    number2 = Mmax;
+}
+else
+{
+    number3 = Mmax;
+}
+if (number3 > number1)
+{
+    number3 = Mmax;
+}
+else
+{
+    number1 = Mmax;
+}
---
+++ задачи/задача 8/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write($"{a} -> ");
+
+for(int q=2; q<=a; q=q+2)
+{
+    Console.WriteLine($"{q}");
+}
