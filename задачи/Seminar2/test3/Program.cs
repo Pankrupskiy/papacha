9671b1a3db619dae696868a1878a8ca5ed06b553
Uladzislau <krupskiy557@gmail.com>
2022-09-12 13:06:53 +0300

    Seminar3

---
+++ задачи/Seminar2/test3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Введите номер недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day<=5)
+{
+    Console.WriteLine($"{day} -> нет" );
+}
+
+else
+{
+    Console.WriteLine($"{day} -> ");
+}
+Console.ReadLine();
+
+
---
+++ задачи/Seminar3/Praktika/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу которая принимает на вход координаты точек (X и Y)
+//Причем X не равен 0 и Y не равен 0
+//выдает четверти плоскости, в которой находится эта точка.
+
+Console.Write("введите X:" );
+int X = int.Parse(Console.ReadLine());
+Console.Write("введите Y:" );
+int Y = int.Parse(Console.ReadLine());
+if(X>0 && Y>0)
+{
+    Console.WriteLine("1");
+}
+if(X<0 && Y>0)
+{
+    Console.WriteLine("2");
+}
+if(X<0 && Y<0)
+{
+    Console.WriteLine("3");
+}
+if(X>0 && Y<0)
+{
+    Console.WriteLine("4");
+}
